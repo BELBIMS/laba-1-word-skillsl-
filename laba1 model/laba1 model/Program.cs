52eb712fae495604ca75e63daa7ec9f85aeb2af4
Душин Максим <60781555+BELBIMS@users.noreply.github.com>
2024-04-03 11:04:42 +0300

    Program.cs

--- laba1 model/laba1 model/Program.cs
+++ laba1 model/laba1 model/Program.cs
@@ -1,16 +1,30 @@
-﻿double n = Convert.ToDouble(Console.ReadLine());
+// Считывание значения n с консоли и преобразование его в тип double
+double n = Convert.ToDouble(Console.ReadLine());
+
+// Считывание значения x с консоли и преобразование его в тип double
 double x = Convert.ToDouble(Console.ReadLine());
+
+// Проверка условия: если абсолютное значение x меньше 1
 if (Math.Abs(x) < 1)
 {
+    // Инициализация переменной result как 0
     double result = 0;
+
+    // тело цикла  с учетом введенного n
     for (double i = 0; i <= n; i++)
     {
+        // тело цикла
         result += Math.Pow(x, 2 * i + 1) / (2 * i + 1);
     }
+
+    // Результат ряда умножается на 2
     result *= 2;
+
+    // Вывод результата на консоль
     Console.WriteLine(result);
 }
 else
 {
+    // В случае если условие не выполнено, выводится сообщение об ошибке
     Console.WriteLine("Error:|х| < 1");
 }
