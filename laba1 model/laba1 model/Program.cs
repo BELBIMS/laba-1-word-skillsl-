ac94600c01ca7fe583ce3d922b347bc3932d9a32
Душин Максим <60781555+BELBIMS@users.noreply.github.com>
2024-04-03 11:04:08 +0300

    Add files via upload

---
+++ laba1 model/laba1 model/Program.cs
@@ -0,0 +1,16 @@
+﻿double n = Convert.ToDouble(Console.ReadLine());
+double x = Convert.ToDouble(Console.ReadLine());
+if (Math.Abs(x) < 1)
+{
+    double result = 0;
+    for (double i = 0; i <= n; i++)
+    {
+        result += Math.Pow(x, 2 * i + 1) / (2 * i + 1);
+    }
+    result *= 2;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Error:|х| < 1");
+}
